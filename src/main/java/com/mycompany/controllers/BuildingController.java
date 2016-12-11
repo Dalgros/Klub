e129ec2c205408569e8525c23c2c1e3cbb861be7
Konrad <ko.dunikowski@gmail.com>
2016-12-11 01:56:57 +0100

    buildings crud

--- src/main/java/com/mycompany/controllers/BuildingController.java
+++ src/main/java/com/mycompany/controllers/BuildingController.java
@@ -5,19 +5,29 @@
  */
 package com.mycompany.controllers;
 
+import com.mycompany.forms.BuildingForm;
+import com.mycompany.forms.ClubForm;
 import com.mycompany.model.Budynek;
+import com.mycompany.model.BudynekAdministracyjny;
+import com.mycompany.model.Klub;
+import com.mycompany.model.ObiektTreningowy;
+import com.mycompany.model.Stadion;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.Valid;
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -46,19 +56,16 @@ public class BuildingController {
         List trainingObjectList = new ArrayList();
 
         for (Budynek b : buildingsList) {
-            log.info("iteracja");
             if (b.getBudynekAdministracyjny() != null) {
                 buildingAdminList.add(b.getBudynekAdministracyjny());
-                log.info("chuj");
             }
             if (b.getStadion() != null) {
                 arenaList.add(b.getStadion());
-                log.info(b.getStadion().getNazwa());
+
             }
             if (b.getObiektTreningowy() != null) {
                 trainingObjectList.add(b.getObiektTreningowy());
             }
-
         }
         session.close();
         model.addAttribute("buildingAdminList", buildingAdminList);
@@ -69,8 +76,135 @@ public class BuildingController {
     }
 
     @RequestMapping(value = "/edit/{idBuilding}", method = RequestMethod.GET)
-    public String buildingEdit(@PathVariable("idClub") String idClub, @PathVariable("idBuilding")String idBuilding, Model model) {
-        log.info(idClub+":"+idBuilding);
+    public String buildingEdit(BuildingForm buildingForm, @PathVariable("idBuilding") String idBuilding, Model model) {
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        //creating session object  
+        Session session = factory.openSession();
+
+        Budynek building = session.find(Budynek.class, Integer.parseInt(idBuilding));
+        model.addAttribute("building", building);
+        if (building.getBudynekAdministracyjny() != null) {
+            model.addAttribute("bAdmin", building.getBudynekAdministracyjny());
+        }
+        if (building.getStadion() != null) {
+            model.addAttribute("bArena", building.getStadion());
+
+        }
+        if (building.getObiektTreningowy() != null) {
+            model.addAttribute("bTrainingObject", building.getObiektTreningowy());
+        }
+
+        session.close();
+        factory.close();
         return "/building/edit_building_view";
+    }
+
+    @RequestMapping(value = "/edit/{idBuilding}", method = RequestMethod.POST)
+    public ModelAndView buildingedit(@Valid BuildingForm buildingForm, @PathVariable("idBuilding") String idBuilding, @PathVariable("idClub") String idClub, Model model) {
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        //creating session object  
+        Session session = factory.openSession();
+
+        Transaction t = session.beginTransaction();
+        Budynek building = session.find(Budynek.class, Integer.parseInt(idBuilding));
+
+        if (building.getStadion() != null) {
+            Stadion arena = session.find(Stadion.class, Integer.parseInt(idBuilding));
+            arena.setDyscyplina(buildingForm.getDiscipline());
+            arena.setIloscMiejsc(Integer.parseInt(buildingForm.getCapacity()));
+            arena.setNazwa(buildingForm.getName());
+            session.update(arena);
+        }
+        if (building.getObiektTreningowy() != null) {
+            model.addAttribute("bTrainingObject", building.getObiektTreningowy());
+        }
+        building.setKodPocztowy(buildingForm.getPostcode());
+        building.setMiejscowosc(buildingForm.getCity());
+        building.setUlicanumer(buildingForm.getStreet());
+
+        session.update(building);
+        t.commit();
+
+        session.close();
+        factory.close();
+
+        return new ModelAndView("redirect:/club/" + idClub + "/buildings/");
+
+    }
+
+    @GetMapping("/remove//{idBuilding}")
+    public ModelAndView removeClub(Model model, @PathVariable("idClub") String idClub, @PathVariable("idBuilding") String idBuilding) {
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        //creating session object  
+        Session session = factory.openSession();
+
+        Transaction t = session.beginTransaction();
+        Budynek building = session.find(Budynek.class, Integer.parseInt(idBuilding));
+        session.remove(building);
+        t.commit();
+        log.info("działa");
+        session.close();
+        factory.close();
+        return new ModelAndView("redirect:/club/" + idClub + "/buildings/");
+    }
+
+    @GetMapping("/create")
+    public String createBuilding(BuildingForm buildingForm, Model model, @PathVariable("idClub") String idClub) {
+        model.addAttribute("Club", idClub);
+        return "/building/create_building_view";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ModelAndView createbuilding(@Valid BuildingForm buildingForm, @PathVariable("idClub") String idClub, Model model) {
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        //creating session object  
+        Session session = factory.openSession();
+
+        Transaction t = session.beginTransaction();
+
+        Klub klub=session.find(Klub.class, Integer.parseInt(idClub));
+        Budynek building = new Budynek();
+        building.setKodPocztowy(buildingForm.getPostcode());
+        building.setMiejscowosc(buildingForm.getCity());
+        building.setUlicanumer(buildingForm.getStreet());
+        building.setIdKlub(klub);
+        session.persist(building);
+
+        if (buildingForm.getCapacity() != null) {
+            Stadion arena = new Stadion(building.getIdbudynek());
+            arena.setDyscyplina(buildingForm.getDiscipline());
+            arena.setIloscMiejsc(Integer.parseInt(buildingForm.getCapacity()));
+            arena.setNazwa(buildingForm.getName());
+            arena.setBudynek(building);
+            session.persist(arena);
+        } else if (buildingForm.getDiscipline() != null) {
+            ObiektTreningowy ot = new ObiektTreningowy(building.getIdbudynek());
+            ot.setBudynek(building);
+            session.persist(ot);
+        } else {
+            BudynekAdministracyjny ba = new BudynekAdministracyjny(building.getIdbudynek());
+            ba.setBudynek(building);
+            session.persist(ba);
+        }
+
+        t.commit();
+
+        session.close();
+        factory.close();
+
+        return new ModelAndView("redirect:/club/" + idClub + "/buildings/");
+
     }
 }
---
+++ src/main/java/com/mycompany/forms/BuildingForm.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.forms;
+
+/**
+ *
+ * @author user
+ */
+public class BuildingForm {
+    
+     private String name;
+     private String city;
+     private String postcode;
+     private String street;
+     private String capacity;
+     private String discipline;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(String discipline) {
+        this.discipline = discipline;
+    }
+    
+    
+}
